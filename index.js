cf5c020d95ae9dd6905fa9171a867a985b744b51
kuzdogan <kaanuzdogan@hotmail.com>
2020-09-14 18:08:38 +0300

    Nice output, remove unnecessary files

--- index.js
+++ index.js
@@ -1,70 +1,61 @@
 const axios = require('axios');
-const { abi } = require('./build/contracts/RelaySet.json');
+const abi = require('./abis/RelaySet.abi.json');
+const metaDataAbi = require('./abis/ValidatorMetadata.abi.json');
 const Web3 = require('web3');
 const provider = new Web3.providers.HttpProvider('https://core.bloxberg.org');
-
 const contractAddress = '0x9850711951A84Ef8a2A31a7868d0dCa34B0661cA';
+const metaDataContractAddress = '0xF2Cde379d6818Db4a8992ed132345e18e99689e9';
+
+const web3 = new Web3(provider);
+const contract = new web3.eth.Contract(abi, contractAddress);
+const metaDataContract = new web3.eth.Contract(metaDataAbi, metaDataContractAddress);
+
+
 
 getValidatorArray();
 
+
+
 async function getValidatorArray() {
-  const web3 = new Web3(provider);
-  const contract = new web3.eth.Contract(abi, contractAddress);
 
   const validators = await contract.methods.getValidators().call();
-  // Check each validator.
-  for (i = 0; i < validators.length; i++) {
+
+  console.log('Online?\tInstitute Name/Address');
+  // Check and print each institute
+  for (let i = 0; i < validators.length; i++) {
     try {
-      // Get latest block mined.
-      let response = await axios.get('https://blockexplorer.bloxberg.org/api/api?module=account&action=getminedblocks&address=' + validators[i] + '&page=1&offset=1')
-      let validatorData = response.data;
+      validatorData = await getLastBlock(validators[i]);
+      instituteName = await getInstituteName(validators[i]);
+      let is24 = false;
       if (validatorData.status == 1) {
         // Check if last block is within 24h.
         let date1 = new Date(validatorData.result[0].timeStamp)
         let timeStamp = Math.round(new Date().getTime() / 1000);
         let timeStampYesterday = timeStamp - (24 * 3600);
-        let is24 = date1 >= new Date(timeStampYesterday * 1000).getTime();
-        console.log(is24, validators[i], i);
-      } else {
-        console.log("No blocks validated", validators[i], i)
+        is24 = date1 >= new Date(timeStampYesterday * 1000).getTime();
       }
+      console.log(`${i + 1}. ${is24 ? '✅' : '❌'}\t${instituteName}`);
     } catch (error) {
       console.log(error)
     }
   }
-  //Unused function below
-  function checkValidatorTime(validatorAddress, counter) {
-    console.log(validatorAddress)
-    // Make a request for a user with a given ID
-    axios.get('https://blockexplorer.bloxberg.org/api/api?module=account&action=getminedblocks&address=' + validatorAddress + '&page=1&offset=1', {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-      },
+}
+
+function getLastBlock(address) {
+  return axios.get('https://blockexplorer.bloxberg.org/api/api?module=account&action=getminedblocks&address=' + address + '&page=1&offset=1')
+    .then(res => {
+      return res.data
     })
-      .then((response) => {
-        if (response.data.status === 1) {
-          var date1 = new Date(response.data.result[0].timeStamp)
-          var timeStamp = Math.round(new Date().getTime() / 1000);
-          var timeStampYesterday = timeStamp - (24 * 3600);
-          var is24 = date1 >= new Date(timeStampYesterday * 1000).getTime();
-          console.log(is24, validatorAddress, counter);
-          return;
-        }
-        else {
-          return;
-        }
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
+}
 
-      })
-      .then(function () {
-        return;
-      });
-
-  }
-
-  getValidatorArray();
+function getInstituteName(address) {
+  return metaDataContract.methods.validatorsMetadata(address).call()
+    .then(result => {
+      // Return name and address if exist
+      if (result.researchInstitute) {
+        return result.researchInstitute + ': ' + address
+      }
+      // Else just the address
+      return address;
+    })
 }
