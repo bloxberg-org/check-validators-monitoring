73693e81c1eda5582724bc37264bc6ab4b03dd27
kuzdogan <kaanuzdogan@hotmail.com>
2020-12-08 12:53:37 +0100

    Seperate validator functions to a module.
    Refactor to have validator retrieval and online check functions in a
    seperate module. The result will return the list of an object with
    validator infos and their last up status. These will be called from the main function
    and passed to the emailer module.
    
    Also npm install nodemailer and dotenv

---
+++ validators.js
@@ -0,0 +1,93 @@
+
+const axios = require('axios');
+const abi = require('./abis/RelaySet.abi.json');
+const metaDataAbi = require('./abis/ValidatorMetadata.abi.json');
+const Web3 = require('web3');
+const provider = new Web3.providers.HttpProvider('https://core.bloxberg.org');
+const contractAddress = '0x9850711951A84Ef8a2A31a7868d0dCa34B0661cA';
+const metaDataContractAddress = '0xF2Cde379d6818Db4a8992ed132345e18e99689e9';
+const web3 = new Web3(provider);
+const contract = new web3.eth.Contract(abi, contractAddress);
+const metaDataContract = new web3.eth.Contract(metaDataAbi, metaDataContractAddress);
+
+/**
+ * @function to check when validators were online.
+ * 
+ * Gets the list of validator addresses from the contract. Then asks the blockexplorer.bloxberg.org when their last validator block was. Check if the last block is within 24h and 14d. 
+ * 
+ * @returns {Object[]} Array of objects for each validator in format:
+ * @example 
+ * [{
+ *  instituteName: String
+ *  address: String,
+ *  lastOnline: Date,
+ *  isUp24h: Boolean,
+ *  isUp14d: Boolean
+ * }, ...]
+ */
+async function getValidatorArray() {
+
+  const validators = await contract.methods.getValidators().call();
+
+  console.log('    24h\t14d\tInstitute Name: Address');
+  console.log('------------------------------------------');
+  // Check and print each institute
+  let resultArray = [];
+  for (let i = 0; i < 3; i++) { // Debug
+  // for (let i = 0; i < validators.length; i++) {
+    let address = validators[i];
+    validatorData = await getLastBlock(address);
+    instituteName = await getInstituteName(address);
+    let isUp24h = false;
+    let isUp14d = false;
+    let lastOnline;
+    if (validatorData.status == 1) {
+      lastOnline = new Date(validatorData.result[0].timeStamp);
+      isUp24h = isDateWithin24h(lastOnline);
+      isUp14d = isDateWithin14d(lastOnline);
+    }
+    let num = i + 1 + '.'; // 2. 14. etc.
+    num = num.padEnd(3, ' '); // Add padding for printing.
+    let displayNameAndAddress = instituteName.slice(0, 31).padEnd(31, ' ') + ': ' + address;
+    console.log(`${num} ${isUp24h ? '✅' : '❌'}\t${isUp14d ? '✅' : '❌'}\t${displayNameAndAddress}`);
+    resultArray[i] = {
+      instituteName,
+      address,
+      lastOnline,
+      isUp24h, 
+      isUp14d
+    }
+  }
+  return resultArray;
+}
+
+getLastBlock = (address) => {
+  return axios.get('https://blockexplorer.bloxberg.org/api/api?module=account&action=getminedblocks&address=' + address + '&page=1&offset=1')
+    .then(res => {
+      return res.data
+    })
+}
+
+getInstituteName = (address) => {
+  return metaDataContract.methods.validatorsMetadata(address).call()
+    .then(result => {
+      // Return name
+        return result.researchInstitute;
+    })
+}
+
+isDateWithin24h = (date) => {
+  let timeStamp = Math.round(new Date().getTime() / 1000);
+  let timeStampYesterday = timeStamp - (1 * 24 * 3600);
+  return date >= new Date(timeStampYesterday * 1000).getTime();
+}
+
+isDateWithin14d = (date) => {
+  let timeStamp = Math.round(new Date().getTime() / 1000);
+  let timeStamp14DaysAgo = timeStamp - (14 * 24 * 3600);
+  return date >= new Date(timeStamp14DaysAgo * 1000).getTime();
+}
+
+module.exports = {
+  getValidatorArray
+}
